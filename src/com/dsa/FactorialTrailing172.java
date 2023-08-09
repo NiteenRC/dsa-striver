219c76a458911d9daf303f720c1266a42bcb1e3b
Satish Chougule <satishrajc@gmail.com>
2023-08-09 20:07:32 +0530

    FactorialTrailing172

--- src/com/dsa/FactorialTrailing172.java
+++ src/com/dsa/FactorialTrailing172.java
@@ -1,2 +1,17 @@
-package com.dsa;public class FactorialTrailing172 {
+package com.dsa;
+
+public class FactorialTrailing172 {
+    public static void main(String[] args) {
+        System.out.println(trailingZeroes(25));
+    }
+
+    public static int trailingZeroes(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
 }
